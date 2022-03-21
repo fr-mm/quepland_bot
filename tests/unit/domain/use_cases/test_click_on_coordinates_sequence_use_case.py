7bab4c990da171530566e0ac27f47984e7602803
chico <chico.zimcolor@gmail.com>
2022-03-21 15:01:20 -0300

    test for ClickOnCoordinatesSequenceUseCase implemented

---
+++ tests/unit/domain/use_cases/test_click_on_coordinates_sequence_use_case.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from mockito import mock, verify, unstub
+
+from src.domain.use_cases import ClickOnCoordinatesSequenceUseCase
+from src.domain.value_objects import Coordinates
+
+
+class TestClickOnCoordinatesSequenceUseCase(TestCase):
+    def tearDown(self) -> None:
+        unstub()
+
+    def test_execute_WHEN_called_THEN_calls_mouse_click_on_with_given_coordinates_collection(self) -> None:
+        seconds_between_clicks = 0
+        coordinates = mock(Coordinates)
+        coordinates_sequence = mock({'next_coordinates': coordinates, 'length': 1})
+        mouse = mock({'click_on': lambda x: None})
+        click_on_coordinates_sequence_service = ClickOnCoordinatesSequenceUseCase(mouse, seconds_between_clicks)
+
+        click_on_coordinates_sequence_service.execute(coordinates_sequence, loop_forever=False)
+
+        verify(mouse).click_on(coordinates)
+
