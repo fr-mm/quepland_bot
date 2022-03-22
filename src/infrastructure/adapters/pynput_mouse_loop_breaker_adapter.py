2dc2d55656f2a4c8e5c05d6f89531d05cdee8226
chico <chico.zimcolor@gmail.com>
2022-03-21 21:37:26 -0300

    PynputMouseLoopBreakerAdapter implemented

---
+++ src/infrastructure/adapters/pynput_mouse_loop_breaker_adapter.py
@@ -0,0 +1,21 @@
+from typing import Callable
+from pynput import keyboard
+
+from src.domain.ports import MouseLoopBreakerPort
+
+
+class PynputMouseLoopBreakerAdapter(MouseLoopBreakerPort):
+    __break_loop_callback: Callable
+    __keyboard_listener: keyboard.Listener
+
+    def __init__(self, break_loop_callback: Callable) -> None:
+        self.__break_loop_callback = break_loop_callback
+
+    def break_when_any_key_is_pressed(self) -> None:
+        self.__keyboard_listener = keyboard.Listener(
+            on_press=lambda key: self.__keyboard_listener.stop()
+        )
+
+    def __break_loop(self) -> None:
+        self.__keyboard_listener.stop()
+        self.__break_loop_callback()
